b2d40ec4cf24ad581ebdd870fd9677d5c47709bc
sayinn <say_inn.mui@fairprice.com.sg>
2015-04-10 13:22:21 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,17 @@
+> household_power_consumption <- read.table("~/household_power_consumption.txt", sep=";", quote="\"")
+>   View(household_power_consumption)
+> data_full <- read.table("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"') 
+> data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+> data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+> rm(data_full)
+> datetime <- paste(as.Date(data$Date), data$Time)
+> data$Datetime <- as.POSIXct(datetime)
+> hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red") 
+> 
+> dev.copy(png, file="plot1.png", height=480, width=480) 
+png 
+  4 
+> 
+> dev.off()
+RStudioGD 
+        2 
